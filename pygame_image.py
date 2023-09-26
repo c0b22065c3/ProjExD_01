0eb617b15ddd62bcb95a50078f6569767e540664
c0b22065c3 <c0b22065c3@edu.teu.ac.jp>
2023-09-26 15:28:28 +0900

    Practice_3

--- pygame_image.py
+++ pygame_image.py
@@ -6,16 +6,20 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kokaton_img = pg.image.load("ex01/fig/3.png")
-    kokaton_img = pg.transform.flip(kokaton_img, True, False)
+    flipped_kokaton_img = pg.transform.flip(kokaton_img, True, False)
+    rotated_kokaton_img = pg.transform.rotozoom(flipped_kokaton_img, 10, 1.0)
+    kokatons_list = [flipped_kokaton_img, rotated_kokaton_img]
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kokaton_img, [300,200])
+        screen.blit(flipped_kokaton_img, [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
