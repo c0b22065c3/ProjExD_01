c4618161a0b0c34537276b45d44a70bffa27c0e2
c0b22065c3 <c0b22065c3@edu.teu.ac.jp>
2023-09-26 16:57:41 +0900

    Exercise_2

--- pygame_image.py
+++ pygame_image.py
@@ -8,6 +8,8 @@ def main():
     clock  = pg.time.Clock()
 
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    rotated_bg_img = pg.transform.flip(bg_img, True, False)
+
     kokaton_img = pg.image.load("ex01/fig/3.png")
     flipped_kokaton_img = pg.transform.flip(kokaton_img, True, False)
     # rotated_kokaton_img = pg.transform.rotozoom(flipped_kokaton_img, 10, 1.0)
@@ -17,17 +19,19 @@ def main():
 
     tmr = 0
     bg_x = 0
+    bg_x_2 = 1600
     ret = False
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         if bg_x <= -1600:
-            bg_x = 0
+            bg_x = 1600
+        if bg_x_2 <= -1600:
+            bg_x_2 = 1600
 
         screen.blit(bg_img, [bg_x, 0])
-        if bg_x <= -800:
-            screen.blit(bg_img, [bg_x + 1600, 0])
+        screen.blit(rotated_bg_img, [bg_x_2, 0])
 
         if ret:
             flap = -1 * (tmr % angle + 1)
@@ -44,6 +48,7 @@ def main():
         pg.display.update()
         tmr += 1        
         bg_x -= 1
+        bg_x_2 -= 1
         clock.tick(100)
 
 
