df9877cf7bd2ef3fb49a33d3529045e1a740470b
c0b22065c3 <c0b22065c3@edu.teu.ac.jp>
2023-09-26 15:21:38 +0900

    Practice_2

--- pygame_image.py
+++ pygame_image.py
@@ -7,12 +7,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    kokaton_img = pg.image.load("ex01/fig/3.png")
+    kokaton_img = pg.transform.flip(kokaton_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kokaton_img, [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
